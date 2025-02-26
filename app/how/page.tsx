a2bb3033a012a9b679461a969d39644ce6047175
Jakob Ossmann <Jakobossmann@gmail.com>
2025-02-26 19:03:02 +1100

    text

--- app/how/page.tsx
+++ app/how/page.tsx
@@ -21,12 +21,41 @@ export default function How() {
         {/* <div className='block md:hidden'>
             <ProgressbarOldMobile />
         </div> */}
-        <div className='hidden md:block mx-44 text-center'>
+
+    <div className='hidden md:block mx-20'>
+        <div className='mx-24 text-center'>
             <ProgressbarNew />
         </div>
+        <div className='mx-24 mt-14'>
+        <p className='text-base mb-4'>
+            Where before it took months to get you to your new application by hiring an agency, you now get your working prototype instantly.
+        </p>
+        <p className='text-base mb-6'>
+            We leverage the latest AI tools to build together with you in interactive agile sessions on real working applications.
+        </p>
+
+        <h2 className='text-2xl mb-4'>1. Introductory Vision Meeting</h2>
+        <p className='text-base mb-6'>
+        In an introductory meeting we get a feeling for your vision and brainstorm ideas together.
+        </p>
+
+        <h2 className='text-2xl mb-4'>2. Live Coding Session</h2>
+        <p className='text-base mb-3'>
+        In Live face-to-face sessions we work with you on real interfaces to bring your ideas to life in real-time.
+        </p>
+        <p className='text-base mb-6'>
+        After every session you end up with a working application. Agile in the age of AI!
+        </p>
+
+        </div>
+    </div>
+
         <div className='block md:hidden mx-12 mt-32 text-center'>
             <ProgressbarNewMobile />
         </div>
+        
+        {/* Empty bottom space */}
+      <div className='p-20'></div>
     </div>
     );
   }
