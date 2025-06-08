e5a8ba116e865e82e72c6d52526bd336087b7fc1
Jakob Ossmann <Jakobossmann@gmail.com>
2025-06-08 21:47:17 +1000

    health bars

--- public/js/index.js
+++ public/js/index.js
@@ -57,9 +57,13 @@ function animate() {
   requestAnimationFrame(animate);
 
   c.drawImage(image, 0, 0)
-  enemies.forEach(enemy => {
+
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i]
     enemy.update()
-  })
+  }
+
+
 
   placementTiles.forEach((tile) => {
     tile.update(mouse)
@@ -85,7 +89,19 @@ function animate() {
         const xDifference = projectile.enemy.center.x - projectile.position.x
         const yDifference = projectile.enemy.center.y - projectile.position.y
         const distance = Math.hypot(xDifference, yDifference)
+
+        // this is when a projectile hits an enemy
         if (distance < projectile.enemy.radius + projectile.radius) {
+            projectile.enemy.health -= 20
+            //remove the enemy when its out of health
+            if (projectile.enemy.health <= 0) {
+                const enemyIndex = enemies.findIndex((enemy) => {
+                    return projectile.enemy === enemy         //we store in enemyindex the current enemy, if it is the one we are looping over
+                })
+
+                if (enemyIndex > -1) enemies.splice(enemyIndex, 1) //remove enemy, if > -1 so if enemy is already dead when hit it doesnt remove other enemy
+            }
+            console.log(projectile.enemy.health)
             building.projectiles.splice(i, 1) 
         }
     }
